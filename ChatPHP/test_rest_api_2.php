f9f3d9e241b5a3414bd86a6bc3374931adb9dda8
Jorik de Boer <jorik_88@hotmail.com>
2018-01-25 16:44:13 +0100

    Nieuwe versie: werkende PUT functie
    De post functie is vervangen door een PUT functie om in de toekomst aan te sluiten op het api protocol

--- ChatPHP/test_rest_api_2.php
+++ ChatPHP/test_rest_api_2.php
@@ -1,5 +1,6 @@
 <?php
     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: *");
     $verb = $_SERVER['REQUEST_METHOD'];
 
     // code om met GET (nieuwe) berichten te krijgen (met de parameters "minimumid" & mykey") 
@@ -29,16 +30,31 @@
 
     // code om met PUT een bericht te maken (met de parameter body, inclusief "id", "mykey" en "value")
     //WORK in PROGRESS
-    elseif ($verb == 'POST'){
+    elseif ($verb == 'PUT'){
         
-        //if (isset( $_POST["username"] )){
-            echo "Username: " .$_POST["username"]. "\n";
-            echo "Message: " .$_POST["message"]. "\n";
-            $mytextfile = fopen("mytextfile.txt", "w") or die("Unable to open file!");
+            /* PUT data comes in on the stdin stream */
+            $putdata = fopen("php://input", "r");
+            parse_str(file_get_contents("php://input"),$post_vars);
+            $mytextfile = fopen("mytextfile.txt", "a+") or die("Unable to open file!");
+            // De oude chatmessages worden bewaard voor nieuwe file
             $old_content = file_get_contents($mytextfile);
-            $txt = "The username = " .$_POST["username"]. "\nThe message = " .$_POST["message"]. "\n";
-            fwrite($mytextfile, $txt."\n".$oldcontent);
+            // De nieuwe content wordt uitgeschreven
+            $new_content = "The username = " .$post_vars['username']. "\nThe message = " .$post_vars['message']. "\n";
+            // Alles wordt teruggezet in de tekstfile
+            fwrite($mytextfile, $new_content."\n".$old_content);
             fclose($mytextfile);
+        
+        //DE OUDE POST MANIER
+        //if (isset( $_POST["username"] )){
+            //echo "Username: " .$_POST["username"]. "\n";
+            //echo "Message: " .$_POST["message"]. "\n";
+            //$mytextfile = fopen("mytextfile.txt", "a+") or die("Unable to open file!");
+            //$old_content = file_get_contents($mytextfile);
+            //$new_content = "The username = " .$_POST["username"]. "\nThe message = " .$_POST["message"]. "\n";
+            //echo $old_content;
+            //echo $new_content;
+            //fwrite($mytextfile, $new_content."\n".$old_content);
+            //fclose($mytextfile);
             //$myfile = fopen("newfile2.txt", "w") or die("Unable to open file!");
             //$txt = $_POST['value'];
             //fwrite($myfile, $txt);
